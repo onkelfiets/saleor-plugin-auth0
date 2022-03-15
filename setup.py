8a3367f6183f709e906aa9c7e72b2b36cf2c7977
Marco Köppel <marco.koeppel@merckgroup.com>
2022-03-15 22:45:35 +0100

    fix: renaming package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     author="Marco Köppel",
     author_email="marco.koeppel@googlemail.com",
-    packages=["auth0-auth"],
-    entry_points={"saleor.plugins": ["auth0-auth = auth0-auth.plugin:Auth0AuthPlugin"]},
+    packages=["auth0_auth"],
+    entry_points={"saleor.plugins": ["auth0_auth = auth0_auth.plugin:Auth0AuthPlugin"]},
     install_requires=["auth0-python"],
 )
